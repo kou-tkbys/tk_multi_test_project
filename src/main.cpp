5905d754da4d595b545c50d3f15d6c54a44e1cf1
tkbys <exec.undefined@gmail.com>
2022-12-04 13:11:30 +0900

    やはりraspberry pi pico でやりたいのでボードと環境を変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <Arduino.h>
 #include <TCA9548.h>
-#include <TM1640.h>
 
 TCA9548 MP(0x70);
-
-// Define a 4-digit display module. Pin suggestions:
-// ESP8266 (Wemos D1): data pin 5 (D1), clock pin 4 (D2)
-// ATtiny44A: data pin 9, clock pin 10 (LED_BUILTIN: 8 on ATTinyCore)
-TM1640 module(9, 10, 16);    // data, clock, 4 digits
 
 void setup() {
 
@@ -56,22 +50,6 @@ void setup() {
 
   Serial.println("done...");
   //put your setup code here, to run once:
-
-
-   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  module.setDisplayToString("HALO");
-  delay(500);                        // wait
-  //module.clearDisplay();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  module.setupDisplay(true, 7);   // sometimes noise on the line may change the intensity level
-  int nTime = ((millis() / 1000) / 60) * 100 + (millis() / 1000) % 60; // minutes+seconds as integer
-  module.setDisplayToDecNumber(nTime, _BV(4)); // display dot on digit 4
-  delay(500);                        // wait
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  module.setDisplayToDecNumber(nTime, _BV(3)); // display colon on digit 3
-  delay(500);                        // wait
-}
+void loop() {}
