e23f4378d3dfbe6a2c3847ba734defc0f2378890
tkbys <exec.undefined@gmail.com>
2022-12-06 21:39:15 +0900

    追加でOLEDも
    ザ・サンプル！

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,32 @@
 #include <TM1640.h>
 // #include <TCA9548.h>
 // TCA9548 MP(0x70);
+#include <SPI.h>
+#include <U8g2lib.h>
+
+
+/**
+ 	GND		|		GND
+	VCC		|		VCC (3.3V)
+	SCL		|		SCK (Pin 9)
+	SDA		|		MOSI (Pin 8)
+	RST		|		Pin 6
+	DC		|		Pin 7
+	CS		|		Pin 4 (PIN_SPI_SS)
+
+  SPI0 SCK	    GP2
+  SPI0 TX(MOSI)	GP3
+  SPI0 RX(MISO)	GP4
+  SPI0 CSn  SS  GP5 
+*/
+#define SH1112_PIN_SCK 2
+#define SH1112_PIN_MOSI 3
+#define SH1112_PIN_MISO 4
+#define SH1112_PIN_CSn 5
+// ↑違った。u8g2の中見てったらデフォルトのR0だったのでこの番号は忘れて。
+#define SH1112_PIN_DC 7
+#define SH1112_PIN_RST 6
+U8G2_SH1122_256X64_F_4W_HW_SPI u8g2(U8G2_R0,PIN_SPI_SS,SH1112_PIN_DC,SH1112_PIN_RST);
 
 // Define a 4-digit display module. Pin suggestions:
 // ESP8266 (Wemos D1): data pin 5 (D1), clock pin 4 (D2)
@@ -17,6 +43,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println(__FILE__);
   Serial.println();
+  u8g2.begin();
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   module.setDisplayToString("HALO");
   delay(500);                        // wait
@@ -34,4 +61,27 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   module.setDisplayToDecNumber(nTime, _BV(3)); // display colon on digit 3
   delay(500);   
+
+  u8g2.clearBuffer();
+	u8g2.setFont(u8g2_font_helvB08_tr);
+	u8g2.drawStr(0, 9, "Hello World");
+	u8g2.drawStr(40, 20, "Hello World");
+	u8g2.drawStr(80, 31, "Hello World");
+	u8g2.drawStr(120, 42, "Hello World");
+	u8g2.drawStr(160, 53, "Hello World");
+	u8g2.drawStr(200, 64, "Hello World");
+	u8g2.sendBuffer();
+	// delay(1000);
+
+	// for (int i = 0; i < 3; ++i)
+	// {
+	// 	for (int j = i; j < 64; j += 3)
+	// 	{
+	// 		u8g2.drawHLine(0, j, 256);
+	// 		u8g2.sendBuffer();
+	// 		delay(10);
+	// 	};
+	// }
+
+	// delay(1000);
 }
